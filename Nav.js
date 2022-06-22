da87ac75ff6b3fc8ff026ac479f5569950fc7735
Mohamed Salah <med.salalah84@gmail.com>
2022-06-22 18:15:23 +0100

    initial commit

---
+++ Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './nav.css'
+const Nav =()=>{
+    return(
+        <div className="navbar">
+        <div className="container">
+           <a href="#" className="logo">Logo</a>
+           <ul>
+           
+            <a href="/home">Home</a>
+
+            <a href="/about">About</a>
+           </ul>
+        </div>
+        </div>
+    )
+}
+export default Nav
